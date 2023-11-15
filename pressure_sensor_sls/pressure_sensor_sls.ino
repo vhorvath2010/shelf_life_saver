2d37efb057234757de1cc2d6337096b28ec96184
Vincent Horvath <vhorvath2010@gmail.com>
2023-11-15 14:15:58 -0500

    add LED strip capability

--- pressure_sensor_sls/pressure_sensor_sls.ino
+++ pressure_sensor_sls/pressure_sensor_sls.ino
@@ -1,10 +1,21 @@
+#include <Adafruit_NeoPixel.h>
+
 // Delay in ms before turning light on
 const unsigned long LED_DELAY = 5000;
 
 // Place indicator length in ms
 const unsigned long PLACED_IND_DUR = 1000;
 
+// PIN for addressable LED strip
 const unsigned int LED_OUT = 12;
+const unsigned int NUM_LEDS = 14; // Change this based on number of LED pixels
+Adafruit_NeoPixel strip(NUM_LEDS, LED_OUT);
+
+// const values for strip colors
+const uint32_t TIMER_UP_COLOR = strip.Color(0, 0, 255);
+const uint32_t PLACED_COLOR = strip.Color(0, 255, 0);
+
+// Switch/Pressure sensor input
 const unsigned int SWITCH_IN = 7;
 
 // Keep track of when an object was last placed (not pressed -> pressed)
@@ -13,9 +24,11 @@ unsigned long last_placed = 0;
 
 void setup() {
   // setup pins
-  pinMode(LED_OUT, OUTPUT);
   pinMode(SWITCH_IN, INPUT);
-  // pressure_sensor_in is already setup as analog input pinMode(pressure_sensor_in, INPUT);
+
+  // init LEDs to off
+  strip.begin();
+  strip.show();
 
   last_placed = millis();
 
@@ -28,12 +41,17 @@ void loop() {
 
   // Light up if placed recently or too long ago
   unsigned int curr = millis();
-  unsigned int diff = curr - last_placed;
-  if (pressed && (diff < PLACED_IND_DUR || diff > LED_DELAY)) {
-    digitalWrite(LED_OUT, HIGH);
+  unsigned int time_since_placed = curr - last_placed;
+  if (pressed && (time_since_placed < PLACED_IND_DUR || time_since_placed > LED_DELAY)) {
+    if (time_since_placed < PLACED_IND_DUR) {
+      strip.fill(PLACED_COLOR);
+    } else if (time_since_placed > LED_DELAY) {
+      strip.fill(TIMER_UP_COLOR);
+    }
   } else {
-    digitalWrite(LED_OUT, LOW);
+    strip.clear();
   }
+  strip.show();
 }
 
 void update_last_placed() {
